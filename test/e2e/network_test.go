343f0835942a6d1ad476f73fc08aec5bcf1997ef
Christian Glombek <cglombek@redhat.com>
2020-09-30 16:53:08 +0200

    [test] Increase retry count in network tests
    The East-West test times out on Azure otherwise.

--- test/e2e/network_test.go
+++ test/e2e/network_test.go
@@ -33,7 +33,7 @@ var (
 	// ubi8Image is the name/location of the linux image we will use for testing
 	ubi8Image = "registry.access.redhat.com/ubi8/ubi:latest"
 	// retryCount is the amount of times we will retry an api operation
-	retryCount = 20
+	retryCount = 60
 	// retryInterval is the interval of time until we retry after a failure
 	retryInterval = 5 * time.Second
 )
